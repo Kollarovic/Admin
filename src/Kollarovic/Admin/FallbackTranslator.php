f74fea73a5e401cf643567536757026790af87c3
Mario <kollarovic@gmail.com>
2025-03-05 17:50:43 +0100

    refactor: add whitespace for improved readability

--- src/Kollarovic/Admin/FallbackTranslator.php
+++ src/Kollarovic/Admin/FallbackTranslator.php
@@ -6,6 +6,7 @@ namespace Kollarovic\Admin;
 
 use Nette\Localization\Translator;
 
+
 class FallbackTranslator implements Translator
 {
 	function translate(\Stringable|string $message, mixed...$parameters): string|\Stringable
